2cedf4721e841854661d8396f4af4d61e88b06aa
Aiden Rocha <126905791+Arocha09@users.noreply.github.com>
2025-02-13 15:58:46 -0500

    Simplistic RAG workflow to start

---
+++ src/chroma_query.py
@@ -0,0 +1,30 @@
+from langchain_chroma import Chroma
+import chromadb
+import openai
+
+
+# client = openai.OpenAI(
+#     base_url="https://api.together.xyz/v1",
+#     api_key= environ["TOGETHER_API_KEY"],
+# )
+
+db_file = chromadb.PersistentClient(path = "chromadb")
+
+collection = db_file.get_collection(name = "resume_embeddings")
+
+query = input("Query: ")
+
+#Embed the query for chroma
+
+
+#input embedded query here
+most_sim = collection.query()
+
+prompt = f"Answer {query}, given {most_sim}"
+
+
+#prompt LLM with prompt
+response = ""
+
+
+#Print Response
